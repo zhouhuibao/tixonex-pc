987d35f99b7616f272ab2e7894a005e596fee301
zhouhuibao <1225204716@qq.com>
2020-04-30 17:48:29 +0800

    提交

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .home{
+    img{
+      border: 1px solid #ccc;
+    }
+  }
+</style>
